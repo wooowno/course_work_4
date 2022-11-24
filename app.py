ea2dcf270cec6b587a39da4f48b954dc43da5c17
wooowno <sorrryks@gmail.com>
2022-11-24 15:16:09 +0300

    app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restx import Api
 
 from config import Config
@@ -10,30 +11,19 @@ from views.genres import genre_ns
 from views.movies import movie_ns
 from views.user import user_ns
 
+app = Flask(__name__)
+app.config.from_object(Config())
 
-def create_app(config_object):
-    application = Flask(__name__)
+db.init_app(app)
+migrate = Migrate(app, db, render_as_batch=True)
+api = Api(app)
 
-    application.config.from_object(config_object)
-    application.app_context().push()
-
-    register_extensions(application)
-
-    return application
-
-
-def register_extensions(application):
-    db.init_app(application)
-    api = Api(application)
-
-    api.add_namespace(director_ns)
-    api.add_namespace(genre_ns)
-    api.add_namespace(movie_ns)
-    api.add_namespace(user_ns)
-    api.add_namespace(auth_ns)
-    api.add_namespace(favorite_ns)
-
+api.add_namespace(director_ns)
+api.add_namespace(genre_ns)
+api.add_namespace(movie_ns)
+api.add_namespace(user_ns)
+api.add_namespace(auth_ns)
+api.add_namespace(favorite_ns)
 
 if __name__ == '__main__':
-    app = create_app(Config())
     app.run(host="localhost", port=10001, debug=True)
